b3025adf4740b87dc784b1ee6c2ec4a183f6d222
JornNeiss <Jornneiss@gmail.com>
2024-12-15 02:37:22 +0100

    Basis UI

--- VoetbalEvents/Controllers/HomeController.cs
+++ VoetbalEvents/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VoetbalEvents.Data;
 using VoetbalEvents.Models;
 
 namespace VoetbalEvents.Controllers
@@ -7,14 +9,27 @@ namespace VoetbalEvents.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        // Voeg de ApplicationDbContext toe via dependency injection
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // Haal de aankomende wedstrijden op
+            var aankomendeWedstrijden = await _context.Wedstrijds
+                .Where(w => w.Datum > DateTime.Now)
+                .OrderBy(w => w.Datum)
+                .Take(5) // Limiteer tot de eerstvolgende 5 wedstrijden
+                .ToListAsync();
+
+            // Zet de aankomende wedstrijden in de ViewData
+            ViewData["AankomendeWedstrijden"] = aankomendeWedstrijden;
+
             return View();
         }
 
