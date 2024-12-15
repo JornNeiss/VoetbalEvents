f5db89a6e0c025b6d171e433a6d8dc17250c90eb
JornNeiss <Jornneiss@gmail.com>
2024-12-15 22:40:09 +0100

    UI Bug opgelost

--- VoetbalEvents/Controllers/HomeController.cs
+++ VoetbalEvents/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using VoetbalEvents.Data;
 using VoetbalEvents.Models;
 
 namespace VoetbalEvents.Controllers
@@ -9,27 +7,14 @@ namespace VoetbalEvents.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDbContext _context;
 
-        // Voeg de ApplicationDbContext toe via dependency injection
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            // Haal de aankomende wedstrijden op
-            var aankomendeWedstrijden = await _context.Wedstrijds
-                .Where(w => w.Datum > DateTime.Now)
-                .OrderBy(w => w.Datum)
-                .Take(5) // Limiteer tot de eerstvolgende 5 wedstrijden
-                .ToListAsync();
-
-            // Zet de aankomende wedstrijden in de ViewData
-            ViewData["AankomendeWedstrijden"] = aankomendeWedstrijden;
-
             return View();
         }
 
